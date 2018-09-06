45544725174cb12bf348fb28e8e83058a8b141ed
Joe DeMaro <demaroj@gmail.com>
2018-09-06 17:19:41 -0500

    tidydata
    down to tidydata set for use in plot exercises.
    
    need to work on time!

--- Week1.R
+++ Week1.R
@@ -8,9 +8,9 @@ unzip(tf, exdir = td <- file.path(tempdir(), "myzip"))
 electricdata <- read.table(file.path(tempdir(), "/myzip/household_power_consumption.txt"), sep=";", header= TRUE)
 
 #add column for date and time to use function on 
-electricdata$newdatetime <- as.Date(paste(electricdata$Date, electricdata$Time))
+electricdata$newdatetime <- as.Date(paste(electricdata$Date, electricdata$Time), format="%d/%m/%Y %H:%M:%S")
 #electricdata$newdate <- as.Date(electricdata$Date, format="%d/%m/%y")
 #electricdata$newtime <- as.POSIXct(strptime(electricdata$Time, "%H:%M:%S"))
 
-tidyset <- subset(electricdata, Date>as.Date("2007-02-01 00:00:00") & Date<as.Date("2007-02-02 23:59:00"))
+tidyset <- subset(electricdata, newdatetime==as.Date("2007-02-01") | newdatetime==as.Date("2007-02-02"))
 
